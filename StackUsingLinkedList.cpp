5835dff7ecba65195ea54a2cb094210b8844fada
PutraRaimarDelvin <delvinputra805@gmail.com>
2026-05-12 11:04:51 +0700

    Menambahkan case peek dan exit

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -105,6 +105,21 @@ int main()
                     cout << "Stack is empty. Cannot pop." << endl;
                 }
                 break;
+            case 3 :
+                if (!stack.isEmpty()) 
+                {
+                    stack.peek();
+                }
+                else
+                {
+                    cout << "Stack is empty. No top value." << endl;
+                }
+                break;
+                
+            case 4 :
+                cout << "Exiting program." << endl;
+                break;
         }
+        cout << endl;
     }
 }
